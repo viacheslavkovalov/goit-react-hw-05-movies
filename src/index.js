a50101135d3b6506a8933006ba594963d7344a62
Viacheslav Kovalov <viacheslav.kovalov1@gmail.com>
2022-02-15 14:12:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
